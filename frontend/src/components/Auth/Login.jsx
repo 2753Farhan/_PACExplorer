0ebe77b43d6ae0dcd8807e33986c281cf61317fc
Md Farhan Islam Shuvro <110192435+2753Farhan@users.noreply.github.com>
2024-10-05 04:40:34 +0600

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://pacexplorer-xw3b.onrender.com/api/v1/user/login",
         { email, password }, // Removed role from request body
         {
           headers: {
