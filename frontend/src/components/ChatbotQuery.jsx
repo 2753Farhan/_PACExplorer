df41bc4878ac2f877d2b765831b97e051bc728bf
Md Farhan Islam Shuvro <110192435+2753Farhan@users.noreply.github.com>
2024-10-05 04:39:39 +0600

    Update ChatbotQuery.jsx

--- frontend/src/components/ChatbotQuery.jsx
+++ frontend/src/components/ChatbotQuery.jsx
@@ -12,7 +12,7 @@ const ChatbotQuery = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post(`http://localhost:4000/api/v1/resume/chatbot/${jobId}`, { inputData });
+            const response = await axios.post(`https://pacexplorer-xw3b.onrender.com/api/v1/resume/chatbot/${jobId}`, { inputData });
             setResponseMessage(response.data.message);
         } catch (error) {
             toast.error(error.response.data.message || 'Something went wrong');
