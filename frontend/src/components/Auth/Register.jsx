e8fbf02b36125ba0424fe9ce50f713dde781e345
Md Farhan Islam Shuvro <110192435+2753Farhan@users.noreply.github.com>
2024-10-05 04:40:52 +0600

    Update Register.jsx

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/register",
+        "https://pacexplorer-xw3b.onrender.com/api/v1/user/register",
         { name, phone, email, password }, // Removed role from request body
         {
           headers: {
